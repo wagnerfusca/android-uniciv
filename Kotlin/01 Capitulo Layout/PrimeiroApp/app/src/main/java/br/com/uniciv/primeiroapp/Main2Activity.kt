998d4d6afcd0a0603e53466131fd627ba3091d00
Wagner Fusca <wagnerfusca@gmail.com>
2019-06-24 01:39:15 -0300

    adicionado projeto 01 layout em kotlin

---
+++ Kotlin/01 Capitulo Layout/PrimeiroApp/app/src/main/java/br/com/uniciv/primeiroapp/Main2Activity.kt
@@ -0,0 +1,27 @@
+package br.com.uniciv.primeiroapp
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+
+import br.edu.unicesumar.primeiroapp.R
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+}
